ab66bf5221bd131719e8357c921c59e69914cd2c
Clearsu <clearwater1219@gmail.com>
2023-03-16 19:45:06 +0900

    Feat: ex02 done

---
+++ CPPmodule01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 19:32:24 by jincpark          #+#    #+#             */
+/*   Updated: 2023/03/16 19:44:14 by jincpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void) {
+	std::string string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "address of the string variable: " << &string << std::endl;
+	std::cout << "address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "address held by stringPTR: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "value of the string variable: " << string << std::endl;
+	std::cout << "value pointed by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "value pointed by stringREF: " << stringREF << std::endl;
+	return (0);
+}
