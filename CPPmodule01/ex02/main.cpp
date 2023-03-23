4f8fe3ab27d20eaf1042ee89d35a8cdf90fcdca1
Clearsu <clearwater1219@gmail.com>
2023-03-23 20:33:38 +0900

    Refactor

--- CPPmodule01/ex02/main.cpp
+++ CPPmodule01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 19:32:24 by jincpark          #+#    #+#             */
-/*   Updated: 2023/03/16 19:49:09 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/03/23 20:28:21 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,11 @@ int	main(void) {
 	std::string	*stringPTR = &string;
 	std::string	&stringREF = string;
 
-	std::cout << "address of the string variable: " << &string << std::endl;
-	std::cout << "address held by stringPTR: " << stringPTR << std::endl;
-	std::cout << "address held by stringREF: " << &stringREF << std::endl;
-	std::cout << std::endl;
-	std::cout << "value of the string variable: " << string << std::endl;
-	std::cout << "value pointed by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "address of the string variable: " << &string << '\n';
+	std::cout << "address held by stringPTR: " << stringPTR << '\n';
+	std::cout << "address held by stringREF: " << &stringREF << "\n\n";
+	std::cout << "value of the string variable: " << string << '\n';
+	std::cout << "value pointed by stringPTR: " << *stringPTR << '\n';
 	std::cout << "value pointed by stringREF: " << stringREF << std::endl;
-	return (0);
+	return 0;
 }
