9c0cc62cf0eed8f553d81c8af524a57bfc5b4929
Clearsu <clearwater1219@gmail.com>
2023-03-27 22:50:29 +0900

    Feat: changed test code

--- CPPmodule02/ex02/main.cpp
+++ CPPmodule02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:38:02 by jincpark          #+#    #+#             */
-/*   Updated: 2023/03/26 16:04:06 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/03/27 22:49:32 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,9 @@ int main(void) {
 	std::cout << "Fixed(5.05f) - Fixed(2): " << e << '\n' << std::endl;
 
 	std::cout << "***testing max and min***" << std::endl;
-	std::cout << "a: " << a << ", b: " << b << std::endl;
-	std::cout << "max(a, b): " << Fixed::max( a, b ) << std::endl;
-	std::cout << "min(a, b): " << Fixed::min( a, b ) << std::endl;
+	Fixed f(-100), g(50.5f);
+	std::cout << "f: " << f << ", g: " << g << std::endl;
+	std::cout << "max(f, g): " << Fixed::max( f, g ) << std::endl;
+	std::cout << "min(f, g): " << Fixed::min( f, g ) << std::endl;
 	return 0;
 }
