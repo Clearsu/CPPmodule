d0b6a28aefb6ce48af50729341c99cf52c4bb160
Clearsu <clearwater1219@gmail.com>
2023-03-28 18:47:01 +0900

    Feat: implementing algorithm

--- CPPmodule02/ex03/bsp.cpp
+++ CPPmodule02/ex03/bsp.cpp
@@ -6,28 +6,37 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 16:25:34 by jincpark          #+#    #+#             */
-/*   Updated: 2023/03/27 23:29:05 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/03/28 15:22:19 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-Fixed	getSlope(const Point& a, const Point& b) {
-	return ((a.getY() - b.getY()) / (a.getX() - b.getX()));
+typedef struct	s_vector {
+	Fixed	x;
+	Fixed	y;
+}	t_vector;
+
+void	setVector(t_vector& v, Fixed x1, Fixed x2, Fixed y1, Fixed y2) {
+	v.x = x2 - x1;
+	v.y = y2 - y1;
 }
 
-Fixed	getYIntercept(Fixed& slope, const Point& p) {
-	return (p.getY() - slope * p.getX());
-}
-
-// y = ax + b
 bool	bsp(Point const a, Point const b, Point const c, Point const point) {
-	Fixed a1(getSlope(a, b));
-	Fixed a2(getSlope(b, c));
-	Fixed a3(getSlope(a, c));
-	Fixed b1(getYIntercept(a1, a));
-	Fixed b2(getYIntercept(a2, b));
-	Fixed b3(getYIntercept(a3, c));
-	`
-	return (true);
+	t_vector	v0, v1, v2;
+	Fixed		u, u1, u2;
+	Fixed		l1, l2, l3;
+
+	setVector(v0, a.getX(), c.getX(), a.getY(), c.getY());
+	setVector(v1, a.getX(), b.getX(), a.getY(), b.getY());
+	setVector(v2, a.getX(), point.getX(), a.getY(), point.getY());
+	u = v1.x * v0.y - v0.x * v1.y;
+	u1 = v2.x * v1.y - v1.x * v2.y;
+	u2 = v0.x * v2.y - v2.x * v0.y;
+	l1 = u1 / u;
+	l2 = u2 / u;
+	l3 = Fixed(1) - l1 - l2;
+	if (l1 > 0 && l2 > 0 && l3 > 0)
+		return true;
+	return false;
 }
