2a403b788da3a2277ec003208eb2080856fb4bd8
Clearsu <clearwater1219@gmail.com>
2023-03-28 20:04:09 +0900

    Yeah

--- CPPmodule02/ex03/bsp.cpp
+++ CPPmodule02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 16:25:34 by jincpark          #+#    #+#             */
-/*   Updated: 2023/03/28 15:22:19 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/03/28 19:07:32 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	setVector(t_vector& v, Fixed x1, Fixed x2, Fixed y1, Fixed y2) {
 bool	bsp(Point const a, Point const b, Point const c, Point const point) {
 	t_vector	v0, v1, v2;
 	Fixed		u, u1, u2;
-	Fixed		l1, l2, l3;
+	float		l1, l2, l3;
 
 	setVector(v0, a.getX(), c.getX(), a.getY(), c.getY());
 	setVector(v1, a.getX(), b.getX(), a.getY(), b.getY());
@@ -33,9 +33,11 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point) {
 	u = v1.x * v0.y - v0.x * v1.y;
 	u1 = v2.x * v1.y - v1.x * v2.y;
 	u2 = v0.x * v2.y - v2.x * v0.y;
-	l1 = u1 / u;
-	l2 = u2 / u;
-	l3 = Fixed(1) - l1 - l2;
+	std::cout << "u: " << u << " u1: " << u1 << " u2: " << u2 << std::endl;
+	l1 = u1.toFloat() / u.toFloat();
+	l2 = u2.toFloat() / u.toFloat();
+	l3 = 1.0f - l1 - l2;
+	std::cout << "l1: " << l1 << " l2: " << l2 << " l3: " << l3 << std::endl;
 	if (l1 > 0 && l2 > 0 && l3 > 0)
 		return true;
 	return false;
--- CPPmodule02/ex03/main.cpp
+++ CPPmodule02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:38:02 by jincpark          #+#    #+#             */
-/*   Updated: 2023/03/27 14:12:53 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/03/28 18:57:41 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 bool	bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main(void) {
-	Point	a(4.5f, 1.0f);
-	Point	b(8.88f, 10.5f);
-	Point	c(2.28f, 9.4f);
-	Point	p(3.12830f, 5.23f);
+	Point	a(0, 0);
+	Point	b(100, 0);
+	Point	c(50, 100);
+	Point	p(50, 50);
 
-	bsp(a, b, c, p);
+	if (bsp(a, b, c, p))
+		std::cout << "the point is in the triangle" << std::endl;
+	else
+		std::cout << "the point is not in the triangle" << std::endl;
 	return 0;
 }
