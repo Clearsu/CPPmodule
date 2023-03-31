99950994022a4e11b4f1a293b63774cd6b42c0cf
Clearsu <clearwater1219@gmail.com>
2023-03-31 19:46:05 +0900

    Fix: fixed error

--- CPPmodule01/ex00/Zombie.cpp
+++ CPPmodule01/ex00/Zombie.cpp
@@ -6,14 +6,14 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:58:54 by jincpark          #+#    #+#             */
-/*   Updated: 2023/03/26 21:14:14 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/03/31 19:42:19 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 void	Zombie::announce(void) {
-	std::cout << name << ": BraiiiiiiinnnzzzZ" << std::endl;
+	std::cout << _name << ": BraiiiiiiinnnzzzZ" << std::endl;
 }
 
 void	Zombie::setName(std::string str) {
