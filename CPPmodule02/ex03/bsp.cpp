e238c8f21c0a557db2676451836ff990e93bb443
Clearsu <clearwater1219@gmail.com>
2023-04-03 17:50:41 +0900

    Norm: fixed norm

--- CPPmodule02/ex03/bsp.cpp
+++ CPPmodule02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 16:25:34 by jincpark          #+#    #+#             */
-/*   Updated: 2023/04/03 16:36:29 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/03 17:48:34 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,10 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point) {
 	Fixed u(v0.getX() * v1.getY() - v1.getX() * v0.getY());
 	Fixed u1(v2.getX() * v1.getY() - v1.getX() * v2.getY());
 	Fixed u2(v0.getX() * v2.getY() - v2.getX() * v0.getY());
-	std::cout << "u: " << u << " u1: " << u1 << " u2: " << u2 << std::endl;
 	Fixed l1(u1 / u);
 	Fixed l2(u2 / u);
 	Fixed l3(Fixed(1)  - l1 - l2);
-	std::cout << "l1: " << l1 << " l2: " << l2 << " l3: " << l3 << std::endl;
+	std::cout << "l1: " << l1 << "  l2: " << l2 << "  l3: " << l3 << std::endl;
 	if (l1 > 0 && l2 > 0 && l3 > 0)
 		return true;
 	return false;
--- CPPmodule02/ex03/main.cpp
+++ CPPmodule02/ex03/main.cpp
@@ -6,12 +6,12 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:38:02 by jincpark          #+#    #+#             */
-/*   Updated: 2023/04/03 16:37:46 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/03 17:49:06 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <Point.hpp>
+#include "Point.hpp"
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point);
 
@@ -19,11 +19,27 @@ int main(void) {
 	Point	a(0, 0);
 	Point	b(10.4f, 0);
 	Point	c(5.5f, 9.283f);
-	Point	p(5.0f, 1);
 
-	if (bsp(a, b, c, p))
-		std::cout << "the point is in the triangle" << std::endl;
+	std::cout << "> point inside of the triangle" << std::endl;
+	Point	p1(5.0f, 1);
+	if (bsp(a, b, c, p1))
+		std::cout << "the point is in the triangle\n" << std::endl;
 	else
-		std::cout << "the point is not in the triangle" << std::endl;
+		std::cout << "the point is not in the triangle\n" << std::endl;
+
+	std::cout << "> point on the edge of the triangle" << std::endl;
+	Point	p2(5.0f, 0);
+	if (bsp(a, b, c, p2))
+		std::cout << "the point is in the triangle\n" << std::endl;
+	else
+		std::cout << "the point is not in the triangle\n" << std::endl;
+
+	std::cout << "> point outside of the triangle" << std::endl;
+	Point	p3(5.0f, 10);
+	if (bsp(a, b, c, p3))
+		std::cout << "the point is in the triangle\n" << std::endl;
+	else
+		std::cout << "the point is not in the triangle\n" << std::endl;
+
 	return 0;
 }
