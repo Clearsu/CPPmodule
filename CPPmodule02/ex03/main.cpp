34456e893d42375f511cf95a96a8bec5c6f2630e
Clearsu <clearwater1219@gmail.com>
2023-04-03 17:33:40 +0900

    testing new values

--- CPPmodule02/ex03/main.cpp
+++ CPPmodule02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:38:02 by jincpark          #+#    #+#             */
-/*   Updated: 2023/03/30 18:11:36 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/03 16:37:46 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(void) {
 	Point	a(0, 0);
 	Point	b(10.4f, 0);
 	Point	c(5.5f, 9.283f);
-	Point	p(5.0f, 2.21f);
+	Point	p(5.0f, 1);
 
 	if (bsp(a, b, c, p))
 		std::cout << "the point is in the triangle" << std::endl;
