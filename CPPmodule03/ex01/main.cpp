720c5de627054a371c162e032581b03fe5b7847f
Clearsu <clearwater1219@gmail.com>
2023-04-06 01:25:01 +0900

    Feat: test code done

--- CPPmodule03/ex01/main.cpp
+++ CPPmodule03/ex01/main.cpp
@@ -6,51 +6,31 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 20:40:51 by jincpark          #+#    #+#             */
-/*   Updated: 2023/04/05 15:21:32 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/06 01:23:35 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include <iostream>
+#include "ScavTrap.hpp"
 
-int	main(void) {
-	/********** ClapTrap **********/
-	ClapTrap a("jincpark");
-	ClapTrap b("sechung");
-	ClapTrap c;
-	ClapTrap d;
-
-	// testing copy constructor and copy assignment constructor
-	c = ClapTrap(a);
-
-	// testing actions
-	a.attack(b.getName());
-	b.takeDamage(a.getAttackDamage());
-	a.attack(b.getName());
-	a.attack(b.getName());
-	a.attack(b.getName());
-	a.attack(b.getName());
-	a.attack(b.getName());
-	a.attack(b.getName());
-	a.attack(b.getName());
-	a.attack(b.getName());
-	a.attack(b.getName());
-	a.attack(b.getName());
-	b.beRepaired(10);
-	b.beRepaired(10);
-	b.beRepaired(10);
-	b.beRepaired(10);
-	b.beRepaired(10);
-	b.beRepaired(10);
-	b.beRepaired(10);
-	b.beRepaired(10);
-	b.beRepaired(10);
-	b.beRepaired(10);
-	b.beRepaired(10);
-	d.takeDamage(1000000);
-	d.attack(a.getName());
-	d.beRepaired(10);
-
-	/********** ScavTrap **********/
-
+int	main(void)
+{
+	std::cout << "### testing constructor and destructor calls ###" << std::endl;
+	{
+		ScavTrap test("test");
+	}
+	std::cout << std::endl;
+	std::cout << "### testing virtual functions ###" << std::endl;
+	{
+		ClapTrap a("jincpark");
+		ScavTrap b("sechung");
+		a.attack(b.getName());
+		b.takeDamage(a.getAttackDamage());
+		b.beRepaired(a.getAttackDamage());
+		b.attack(a.getName());
+		a.takeDamage(b.getAttackDamage());
+		a.beRepaired(10);
+		b.guardGate();
+	}
 	return 0;
 }
