130d45b37e1a611e5f915405dec2690c2ab6a7c6
Clearsu <clearwater1219@gmail.com>
2023-04-06 17:55:48 +0900

    Fix: now all function definitions are in .cpp file

--- CPPmodule02/ex01/Fixed.cpp
+++ CPPmodule02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:38:29 by jincpark          #+#    #+#             */
-/*   Updated: 2023/03/26 21:18:19 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/06 17:52:47 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,45 @@
 #include "Fixed.hpp"
 
 /******************** OCF ********************/
-Fixed&	Fixed::operator=(const Fixed& f) {
-	std::cout << "Copy assignment operator called\n";
-	_rawBits = f._rawBits;
-	return *this;
+Fixed::Fixed() : _rawBits(0) {
+	std::cout << "Default constructor called" << std::endl;
 }
 
+Fixed::Fixed(const Fixed &f) : _rawBits(f._rawBits) {
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=(const Fixed &f) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	_rawBits = f._rawBits;
+	return (*this);
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+
 /******************** other constructors ********************/
-Fixed::Fixed(const int value) {
-	std::cout << "Int constructor called\n";
-	_rawBits = value;
+Fixed::Fixed(const int value) : _rawBits(value) {
+	std::cout << "Int constructor called" << std::endl;
 	_rawBits = _rawBits << _fracBits;
 }
 
 Fixed::Fixed(const float value) {
-	std::cout << "Float constructor called\n";
+	std::cout << "Float constructor called" << std::endl;
 	_rawBits = roundf(value * (1 << _fracBits));
 }
 
 
 /******************** member functions ********************/
 int	Fixed::getRawBits(void) const {
-	std::cout << "getRawBits member function called\n";
+	std::cout << "getRawBits member function called" << std::endl;
 	return _rawBits;
 }
 
 void	Fixed::setRawBits(int const rawBits) {
-	std::cout << "setRawBits member function called\n";
+	std::cout << "setRawBits member function called" << std::endl;
 	_rawBits = rawBits;
 }
 
--- CPPmodule02/ex01/Fixed.hpp
+++ CPPmodule02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:38:11 by jincpark          #+#    #+#             */
-/*   Updated: 2023/03/26 16:52:37 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/06 17:53:11 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,10 @@ class	Fixed {
 		static const int	_fracBits = 8;
 	public :
 		//OCF
-		Fixed() : _rawBits(0) {
-			std::cout << "Default constructor called\n";
-		}
-		Fixed(const Fixed &f) : _rawBits(f._rawBits) {
-			std::cout << "Copy constructor called\n";
-		}
-		Fixed&	operator=(const Fixed& f);
-		~Fixed() {
-			std::cout << "Destructor called\n";
-		}
+		Fixed();
+		Fixed(const Fixed &f);
+		Fixed&	operator=(const Fixed &f);
+		~Fixed();
 
 		// other constructors
 		Fixed(const int value);
