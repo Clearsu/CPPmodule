1ae2839e14774ab21bcff7fee8892b2d2ec3ddff
Clearsu <clearwater1219@gmail.com>
2023-04-13 15:03:57 +0900

    Feat: added test code

--- CPPmodule03/ex01/main.cpp
+++ CPPmodule03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 20:40:51 by jincpark          #+#    #+#             */
-/*   Updated: 2023/04/12 18:27:40 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/13 14:47:09 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,22 @@
 
 int	main(void)
 {
-	std::cout << "### testing constructor and destructor calls ###" << std::endl;
+	std::cout << "### testing basic features ###" << std::endl;
 	{
-		ScavTrap test1("test1");
-		ScavTrap test2(test1);
-		std::cout << test2.getHitPoints() << std::endl;
-		std::cout << test2.getEnergyPoints() << std::endl;
-		std::cout << test2.getAttackDamage() << std::endl;
+		{
+			ScavTrap test1("test1");
+			ScavTrap test2(test1);
+			std::cout << test2.getHitPoints() << std::endl;
+			std::cout << test2.getEnergyPoints() << std::endl;
+			std::cout << test2.getAttackDamage() << std::endl;
+		}
+		std::cout << std::endl;
+		ClapTrap *test1 = new ClapTrap("test1");
+		ClapTrap *test2 = new ScavTrap("test2");
+		test1->attack(test2->getName());
+		test2->attack(test1->getName());
+		delete test1;
+		delete test2;
 	}
 	std::cout << std::endl;
 	std::cout << "### testing virtual functions ###" << std::endl;
