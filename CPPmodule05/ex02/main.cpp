708d5c7d2e136fa794fefe0cd9f74a89056033a9
Clearsu <clearwater1219@gmail.com>
2023-04-19 16:14:36 +0900

    Feat: added testcode for RobotomyRequestForm

--- CPPmodule05/ex02/main.cpp
+++ CPPmodule05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 19:34:58 by jincpark          #+#    #+#             */
-/*   Updated: 2023/04/19 13:31:40 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/19 16:12:14 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,38 @@
 
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int	main(void)
 {
+	std::cout << "*** testing ShrubberyCreationForm ***" << std::endl;
+	try
 	{
-		try
-		{
-			Bureaucrat jin("Jin", 1);
-			AForm*	form = new ShrubberyCreationForm("here");
-			form->beSigned(jin);
-			jin.signForm(*form);
-			jin.executeForm(*form);
-			delete form;
-		}
-		catch (std::exception& s)
-		{
-			std::cerr << s.what() << std::endl;
-		}
+		Bureaucrat jin("Jin", 1);
+		AForm*	form = new ShrubberyCreationForm("here");
+		form->beSigned(jin);
+		jin.signForm(*form);
+		jin.executeForm(*form);
+		delete form;
+	}
+	catch (std::exception& s)
+	{
+		std::cerr << s.what() << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << "*** testing RobotomyRequestForm ***" << std::endl;
+	try
+	{
+		Bureaucrat jin("Jin", 1);
+		AForm*	form = new RobotomyRequestForm(jin.getName());
+		form->beSigned(jin);
+		jin.signForm(*form);
+		jin.executeForm(*form);
+		delete form;
+	}
+	catch (std::exception& s)
+	{
+		std::cerr << s.what() << std::endl;
 	}
 	return 0;
 }
