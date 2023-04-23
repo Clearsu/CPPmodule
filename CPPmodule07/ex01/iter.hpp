aada2297898f7eed59a9bb0294a1fcc3a820823c
Clearsu <clearwater1219@gmail.com>
2023-04-23 18:55:11 +0900

    Fix: fixed parameter of iter()

--- CPPmodule07/ex01/iter.hpp
+++ CPPmodule07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 15:43:46 by jincpark          #+#    #+#             */
-/*   Updated: 2023/04/23 16:34:59 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/23 17:31:55 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 # define ITER_HPP
 
 template<typename T>
-void	iter(T* arr, size_t N, void (*func)(T*))
+void	iter(T* arr, size_t N, void (*func)(const T&))
 {
 	for (size_t	i = 0; i < N; i++)
-		func(&arr[i]);
+		func(arr[i]);
 }
 
 #endif
--- CPPmodule07/ex01/main.cpp
+++ CPPmodule07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 15:54:14 by jincpark          #+#    #+#             */
-/*   Updated: 2023/04/23 16:27:27 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/23 17:31:39 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,9 @@
 #define LEN 5
 
 template<typename T>
-void	increase(T* val)
+void	printElement(const T& val)
 {
-	*val += 1;
-}
-
-template<typename T>
-void	printElement(T* val)
-{
-	std::cout << *val << " ";
+	std::cout << "value: " << val << std::endl;
 }
 
 int main(void)
@@ -32,28 +26,16 @@ int main(void)
 	{
 		int	a[LEN] = {1, 2, 3, 4, 5};
 		::iter<int>(a, LEN, printElement<int>);
-		std::cout << std::endl;
-		::iter<int>(a, LEN, increase<int>);
-		::iter<int>(a, LEN, printElement<int>);
-		std::cout << std::endl;
 	}
 	std::cout << std::endl;
 	{
 		char a[LEN] = {'a', 'b', 'c', 'd', 'e'};
 		::iter<char>(a, LEN, printElement<char>);
-		std::cout << std::endl;
-		::iter<char>(a, LEN, increase<char>);
-		::iter<char>(a, LEN, printElement<char>);
-		std::cout << std::endl;
 	}
 	std::cout << std::endl;
 	{
 		float a[LEN] = {1.5f, 2.5f, 3.5f, 4.5f, 5.5f};
 		::iter<float>(a, LEN, printElement<float>);
-		std::cout << std::endl;
-		::iter<float>(a, LEN, increase<float>);
-		::iter<float>(a, LEN, printElement<float>);
-		std::cout << std::endl;
 	}
     return 0;
 }
