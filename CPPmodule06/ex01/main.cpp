44dd368b18b660e13e53305a69c65ad38abd6202
Clearsu <clearwater1219@gmail.com>
2023-04-25 10:17:01 +0900

    Feat: setw() added

--- CPPmodule06/ex01/main.cpp
+++ CPPmodule06/ex01/main.cpp
@@ -6,30 +6,44 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 13:48:46 by jincpark          #+#    #+#             */
-/*   Updated: 2023/04/24 15:17:27 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/04/25 10:08:43 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstdlib>
 #include <stdexcept>
+#include <iomanip>
 
 #include "Serializer.hpp"
+
+#define W 30
 
 int	test(void)
 {
 	try
 	{
 		Data* ptr = new Data;
-		std::cout << "ptr: " << ptr << std::endl;
 		uintptr_t s_ptr = Serializer::serialize(ptr);
-		std::cout << "serialized ptr: " << s_ptr << std::endl;
-		Data* ds_ptr = Serializer::deserialize(s_ptr);
-		std::cout << "deserialized ptr: " << ds_ptr << std::endl;
 		unsigned long ul_ptr = reinterpret_cast<unsigned long>(ptr);
-		std::cout << "ptr casted to unsigned long: " << ul_ptr << std::endl;
+		Data* ds_ptr = Serializer::deserialize(s_ptr);
 		Data* origin_ptr = reinterpret_cast<Data*>(ul_ptr);
-		std::cout << "ptr casted back to Data*: " << origin_ptr << std::endl;
+
+		std::cout << std::setw(W) << "ptr: ";
+		std::cout << ptr << std::endl;
+
+		std::cout << std::setw(W) << "serialized ptr: ";
+		std::cout << s_ptr << std::endl;
+
+		std::cout << std::setw(W) << "deserialized ptr: ";
+		std::cout << ds_ptr << std::endl;
+
+		std::cout << std::setw(W) << "ptr casted to unsigned long: ";
+		std::cout << ul_ptr << std::endl;
+
+		std::cout << std::setw(W) << "ptr casted back to Data*: ";
+		std::cout << origin_ptr << std::endl;
+
 //		unsigned long test = static_cast<unsigned long>(ptr);
 		delete ptr;
 	}
