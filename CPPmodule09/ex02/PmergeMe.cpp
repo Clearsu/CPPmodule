39ff6765533f84d029f5b6002ab2beff5951be37
Clearsu <clearwater1219@gmail.com>
2023-05-02 08:00:59 +0900

    Remove: removed functions to rebuild

--- CPPmodule09/ex02/PmergeMe.cpp
+++ CPPmodule09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 08:14:15 by jincpark          #+#    #+#             */
-/*   Updated: 2023/05/01 15:26:11 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/05/01 21:30:40 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,31 +39,6 @@ void	PmergeMe::parseArgv(int argc, char** argv) {
 	}
 }
 
-void	PmergeMe::mergeInsertionSort(std::vector<int>::iterator start, std::vector<int>::iterator finish) {
-	if (finish - start < 10)
-		insertionSortVector(start, finish);
-	else {
-		
-	}
-}
-
-void	PmergeMe::insertionSortVector(std::vector<int>::iterator start, std::vector<int>::iterator finish) {
-	std::vector<int>::iterator	it_i, it_j;
-	int	key;
-
-	it_i = start + 1;
-	while (it_i != finish) {
-		key = *it_i;
-		it_j = it_i - 1;
-		while (it_j >= start && *it_j > key) {
-			*(it_j + 1) = *it_j;
-			--it_j;
-		}
-		*(it_j + 1) = key;
-		++it_i;
-	}
-}
-
 PmergeMe*	PmergeMe::getInstance(int argc, char** argv) {
 	if (PmergeMe::instance == 0)
 		PmergeMe::instance = new PmergeMe(argc, argv);
@@ -72,26 +47,4 @@ PmergeMe*	PmergeMe::getInstance(int argc, char** argv) {
 
 void	PmergeMe::deleteInstance(void) {
 	delete PmergeMe::instance;
-}
-
-void	PmergeMe::sort(void) {
-	mergeInsertionSort(this->vec_src.begin(), this->vec_src.end());
-}
-
-void	PmergeMe::printSrcVector(void) {
-	std::vector<int>::iterator it = this->vec_src.begin();
-	std::vector<int>::iterator end = this->vec_src.end();
-
-	while (it != end)
-		std::cout << *(it++) << " ";
-	std::cout << std::endl;
-}
-
-void	PmergeMe::printDestVector(void) {
-	std::vector<int>::iterator it = this->vec_dest.begin();
-	std::vector<int>::iterator end = this->vec_dest.end();
-
-	while (it != end)
-		std::cout << *(it++) << " ";
-	std::cout << std::endl;
 }
--- CPPmodule09/ex02/PmergeMe.hpp
+++ CPPmodule09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 15:43:14 by jincpark          #+#    #+#             */
-/*   Updated: 2023/05/01 15:04:15 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/05/01 21:30:53 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,9 @@ class	PmergeMe {
 		~PmergeMe();
 
 		void	parseArgv(int argc, char** argv);
-		void	mergeInsertionSort(std::vector<int>::iterator start,
-				std::vector<int>::iterator finish);
-		void	insertionSortVector(std::vector<int>::iterator start,
-				std::vector<int>::iterator finish);
 	public :
 		static PmergeMe*	getInstance(int argc, char** argv);
 		static void			deleteInstance(void);
-
-		void	sort(void);
-		void	printVector(void);
 };
 
 #endif
