31012d143131845c548fcf0e73cb3913938319d9
Clearsu <clearwater1219@gmail.com>
2023-05-04 20:57:13 +0900

    Fix: fixed compile error

--- CPPmodule09/ex00/main.cpp
+++ CPPmodule09/ex00/main.cpp
@@ -6,18 +6,15 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 17:23:35 by jincpark          #+#    #+#             */
-/*   Updated: 2023/05/01 08:25:13 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/05/04 20:53:17 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdlib>
+//#include <cstdlib>
 #include <stdexcept>
 #include <iostream>
-#include <BitcoinExchange.hpp>
 
-void checkLeak(void) {
-	std::system("leaks btc");
-}
+#include "BitcoinExchange.hpp"
 
 int	main(int argc, char** argv) {
 	if (argc != 2) {
@@ -31,6 +28,6 @@ int	main(int argc, char** argv) {
 	} catch (std::exception& e) {
 		std::cout << e.what() << '\n';
 	}
-//	std::atexit(checkLeak);
+//	std::system("leaks btc");
 	return 0;
 }
