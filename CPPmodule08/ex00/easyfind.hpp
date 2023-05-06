af0827ecf143b15a5815f45f080b4232e0790cf7
Clearsu <clearwater1219@gmail.com>
2023-05-06 16:09:03 +0900

    Fix: now using find() in algorithm library

--- CPPmodule08/ex00/easyfind.hpp
+++ CPPmodule08/ex00/easyfind.hpp
@@ -6,24 +6,28 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 11:02:28 by jincpark          #+#    #+#             */
-/*   Updated: 2023/04/25 17:03:27 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/05/06 16:07:46 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __EASYFIND_HPP__
 #define __EASYFIND_HPP__
 
-template<typename T>
-bool	easyfind(const T& container, int n)
-{
-	typename T::const_iterator iter = container.begin();
-	typename T::const_iterator end = container.end();
+#include <algorithm>
 
+template<typename T>
+bool	easyfind(const T& container, const int n)
+{
+	/*
 	while (iter != end) {
 		if (*iter == n)
 			return true;
 		++iter;
 	}
+	return false;
+	*/
+	if (find(container.begin(), container.end(), n) != container.end())
+		return true;
 	return false;
 }
 
